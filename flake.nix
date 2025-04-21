590845dd2bb6bef232950b4aefdda85404cf4d09
promiscunix <135283400+promiscunix@users.noreply.github.com>
2025-04-20 20:44:28 -0700

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -7,28 +7,37 @@
   };
 
   outputs = { self, nixpkgs, flake-utils, ... }:
-    flake-utils.lib.eachDefaultSystem (system:
-      let
-        pkgs = import nixpkgs { inherit system; };
-      in {
-        packages.tailnet-ping = pkgs.stdenv.mkDerivation {
-          name = "tailnet-ping";
-          src = ./.;
-          nativeBuildInputs = [ pkgs.makeWrapper ];
-          buildPhase = "true";
-          installPhase = ''
-            mkdir -p $out/bin
-            cp tailnet-ping.sh $out/bin/tailnet-ping
-            chmod +x $out/bin/tailnet-ping
-            wrapProgram $out/bin/tailnet-ping \
-              --prefix PATH : ${pkgs.lib.makeBinPath [ pkgs.jq pkgs.tailscale pkgs.iputils ]}
-          '';
-        };
+  flake-utils.lib.eachDefaultSystem (system:
+    let
+      pkgs = import nixpkgs { inherit system; };
+    in {
+      packages.default = pkgs.stdenv.mkDerivation {
+        name = "tailnet-ping";
+        src = ./.;
+        nativeBuildInputs = [ pkgs.makeWrapper ];
+        buildPhase = "true";
+        installPhase = ''
+          mkdir -p $out/bin
+          cp tailnet-ping.sh $out/bin/tailnet-ping
+          chmod +x $out/bin/tailnet-ping
+          wrapProgram $out/bin/tailnet-ping \
+            --prefix PATH : ${pkgs.lib.makeBinPath [ pkgs.jq pkgs.tailscale pkgs.iputils ]}
+        '';
+      };
 
-        apps.tailnet-ping = {
-          type = "app";
-          program = "${self.packages.${system}.tailnet-ping}/bin/tailnet-ping";
-          defaultApp = self.apps.${system}.tailnet-ping;
-        };
-      });
+      apps.default = {
+        type = "app";
+        program = "${self.packages.${system}.default}/bin/tailnet-ping";
+      };
+
+      devShells.default = pkgs.mkShell {
+        packages = [ pkgs.jq pkgs.tailscale pkgs.iputils ];
+
+        shellHook = ''
+          echo "🧪 Welcome to the tailnet-ping dev shell!"
+          echo "Tools available: jq, tailscale, ping"
+        '';
+      };
+    });
+
 }
